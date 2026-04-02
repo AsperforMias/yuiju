01e4774314fa27dbfceb8d821b38051acfc555c1
yixiaojiu <yi_xiao_jiu@qq.com>
2026-04-03 05:32:24 +0800

    fix: implement demo script for validating NapCat WebSocket connectivity

--- packages/message/src/server.ts
+++ packages/message/src/server.ts
@@ -9,22 +9,20 @@ const napcat = new NCWebsocket(config.message.napcat);
 
 napcat.on("message.private", (context) => privateMessageHandler(context, napcat));
 
-// napcat.on("message.group", (context) => groupMessageHandler(context, napcat));
-napcat.on("message.group", (context) => {
-  // console.log(context.message);
-  napcat.send_group_msg({
-    group_id: context.group_id,
-    // user_id: context.sender.user_id,
-    message: [
-      Structs.image(
-        "https://multimedia.nt.qq.com.cn/download?appid=1407&fileid=EhT7w3hcR9IZRi2ta1gTNkZNhgXoDBj8glMg_wootp-H29rPkwMyBHByb2RQgL2jAVoQtEdkW0jyfuM-kZA4tq1E8XoCl-GCAQJneg&rkey=CAMSMN8QDFtI_3a277Tu1aAQElBh4kHOnKbYV6dKq7Zjiz7V66xXeaGLsj31wazWmHqUUg",
-        "动画表情",
-      ),
-    ],
-  });
-});
-
-napcat.fetch_custom_face();
+napcat.on("message.group", (context) => groupMessageHandler(context, napcat));
+// napcat.on("message.group", (context) => {
+//   // console.log(context.message);
+//   napcat.send_group_msg({
+//     group_id: context.group_id,
+//     // user_id: context.sender.user_id,
+//     message: [
+//       Structs.image(
+//         "https://multimedia.nt.qq.com.cn/download?appid=1407&fileid=EhT7w3hcR9IZRi2ta1gTNkZNhgXoDBj8glMg_wootp-H29rPkwMyBHByb2RQgL2jAVoQtEdkW0jyfuM-kZA4tq1E8XoCl-GCAQJneg&rkey=CAMSMN8QDFtI_3a277Tu1aAQElBh4kHOnKbYV6dKq7Zjiz7V66xXeaGLsj31wazWmHqUUg",
+//         "动画表情",
+//       ),
+//     ],
+//   });
+// });
 
 async function main() {
   await connectDB();
